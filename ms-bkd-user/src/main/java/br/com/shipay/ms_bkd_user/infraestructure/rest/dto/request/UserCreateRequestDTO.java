45214a96fb461e70b581c3842c51fce4a08182f1
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2026-03-16 22:38:24 -0300

    adicionando documentações do swagger no json de request e endpoint do usuario

--- ms-bkd-user/src/main/java/br/com/shipay/ms_bkd_user/infraestructure/rest/dto/request/UserCreateRequestDTO.java
+++ ms-bkd-user/src/main/java/br/com/shipay/ms_bkd_user/infraestructure/rest/dto/request/UserCreateRequestDTO.java
@@ -1,21 +1,27 @@
 package br.com.shipay.ms_bkd_user.infraestructure.rest.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "Objeto de requisição para criação de um usuário")
 public record UserCreateRequestDTO (
 
+        @Schema(description = "Nome do usuário", example = "João da Silva")
         @NotBlank(message = "O nome é obrigatório")
         String name,
 
+        @Schema(description = "E-mail do usuário", example = "joao.silva@exemplo.com")
         @NotBlank(message = "O e-mail é obrigatório")
         @Email(message = "Formato de e-mail inválido")
         String email,
 
+        @Schema(description = "ID do papel (Role) do usuário", example = "1")
         @NotNull(message = "O ID do papel (Role) é obrigatório")
         Integer roleId,
 
+        @Schema(description = "Senha do usuário", example = "Shipay123!")
         String password
 ) {
 }
--- ms-bkd-user/src/main/java/br/com/shipay/ms_bkd_user/infraestructure/rest/dto/response/UserResponseDTO.java
+++ ms-bkd-user/src/main/java/br/com/shipay/ms_bkd_user/infraestructure/rest/dto/response/UserResponseDTO.java
@@ -1,12 +1,24 @@
 package br.com.shipay.ms_bkd_user.infraestructure.rest.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 
+@Schema(description = "Objeto de resposta com os dados de um usuário")
 public record UserResponseDTO(
+        @Schema(description = "ID único do usuário", example = "100")
         Long id,
+
+        @Schema(description = "Nome do usuário", example = "João da Silva")
         String name,
+
+        @Schema(description = "E-mail do usuário", example = "joao.silva@exemplo.com")
         String email,
+
+        @Schema(description = "ID do papel (Role) do usuário", example = "1")
         Integer roleId,
+
+        @Schema(description = "Data de criação do usuário", example = "2024-10-25")
         LocalDate createdAt
 ) {
 }
